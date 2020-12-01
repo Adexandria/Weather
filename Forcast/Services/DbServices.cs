d203a3b67283a7618f001d7a46deb82aee1c01c6
Adeola Aderibigbe <adeolaaderibigbe09@gmail.com>
2020-12-01 12:08:20 +0100

    Added dbcontext and repos

---
+++ Forcast/Services/DbServices.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Forcast.Services
+{
+    public class DbServices: DbContext 
+    {
+        public DbServices(DbContextOptions<DbServices> options):base(options)
+        {
+
+        }
+       public DbSet<WeatherForecast> forecasts { get; set; }
+    }
+}
---
+++ Forcast/Services/Weather.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Forcast.Services
+{
+    public class Weather :IWeather
+    {
+        private readonly DbServices db;
+        public Weather(DbServices db)
+        {
+            this.db = db;
+        }
+        public IEnumerable<WeatherForecast> Weathers 
+        {
+            get
+            {
+                return db.forecasts.OrderBy(r => r.Date);
+            }
+        }
+
+    }
+}
