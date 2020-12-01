5d9ece8907be2509e999c3524a0378d118b141f9
Adeola Aderibigbe <adeolaaderibigbe09@gmail.com>
2020-12-01 12:29:57 +0100

    Added profile and Dto

---
+++ Forcast/WeatherProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Forcast
+{
+    public class WeatherProfile :Profile
+    {
+        public WeatherProfile()
+        {
+            CreateMap<WeatherDTO, WeatherForecast>();
+
+        }
+    }
+}
