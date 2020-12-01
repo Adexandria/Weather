f6f4aae826575df1f44fa9856cf09c47357adb5d
Adeola Aderibigbe <adeolaaderibigbe09@gmail.com>
2020-12-01 12:32:33 +0100

    added the controller

--- Forcast/Controllers/WeatherForecastController.cs
+++ Forcast/Controllers/WeatherForecastController.cs
@@ -2,38 +2,32 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AutoMapper;
+using Forcast.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
 namespace Forcast.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("api/weather")]
     public class WeatherForecastController : ControllerBase
     {
-        private static readonly string[] Summaries = new[]
-        {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        };
 
-        private readonly ILogger<WeatherForecastController> _logger;
-
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        private readonly IWeather weather;
+        private readonly IMapper mapper;
+        public WeatherForecastController(IWeather weather, IMapper mapper)
         {
-            _logger = logger;
+            this.weather = weather;
+            this.mapper = mapper;
         }
 
         [HttpGet]
-        public IEnumerable<WeatherForecast> Get()
+        public ActionResult Get()
         {
-            var rng = new Random();
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = rng.Next(-20, 55),
-                Summary = Summaries[rng.Next(Summaries.Length)]
-            })
-            .ToArray();
+            var forecast = weather.Weathers;
+            var newforcast = mapper.Map<IEnumerable<WeatherDTO>>(forecast);
+            return Ok(newforcast);
         }
     }
 }
